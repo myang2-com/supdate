02d5b6b436f11b30dc7e73646e0d69e1648a8f69
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-06-02 16:10:59 +0900

    Remove Package.scan(), .build()

--- supdate/package.py
+++ supdate/package.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import shutil
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import List, Optional, Callable
+from typing import List, Optional, Dict, Any
 from urllib.parse import urljoin
 
 from .profile import Profile
@@ -33,32 +33,3 @@ class Package(Profile):
     @classmethod
     def from_profile(cls, profile: Profile) -> Package:
         return cls.from_json(profile.to_json())
-
-    def scan(self, instance_folder):
-        for file in instance_folder.rglob("**/*"):
-            if not file.is_file():
-                continue
-
-            path: Path = file.relative_to(instance_folder)
-            yield file, path
-
-    def build(self, instance_folder: Path, url: str, target_folder: Path = None,
-              func: Callable[[Path, Path], bool] = None):
-        for file, path in self.scan(instance_folder):
-            if func:
-                result = func(file, path)
-                if result is not None and not result:
-                    continue
-
-            if target_folder is not None:
-                target_path: Path = target_folder / path
-                target_path.parent.mkdir(parents=True, exist_ok=True)
-                shutil.copyfile(str(file), str(target_path))
-
-            file_stat = file.stat()
-            self.files.append(PackageFile(
-                size=file_stat.st_size,
-                sha1=sha256_hexdigest(file),
-                path=path.as_posix(),
-                url=urljoin(url, file.relative_to(instance_folder).as_posix())
-            ))
