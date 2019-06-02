18c1f8447d14cc794641855f4240d1e892c799bd
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-06-02 16:09:26 +0900

    Add IndexPackage, Launcher, IndexPackageManifest for nupdate like updater

---
+++ supdate/index.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Dict
+
+from .package import Package
+from .typed import Namespace
+from .utils import sha256_hexdigest
+
+
+@dataclass
+class IndexPackage(Namespace):
+    name: str
+    version: str
+    time: str
+    url: str
+    path: str
+    sha1: str
+    size: int
+
+    @classmethod
+    def from_package(cls, package: Package, modpack_path: Path, package_url: str):
+        assert modpack_path.name == "modpack.json", modpack_path
+        st = modpack_path.stat()
+
+        return cls(
+            name=package.name,
+            version=package.version,
+            time=package.time,
+            url=package_url,
+            path="modpack.json",
+            sha1=sha256_hexdigest(modpack_path),
+            size=st.st_size,
+        )
+
+
+@dataclass
+class Launcher(Namespace):
+    version: str
+    url: str
+
+
+@dataclass
+class IndexPackageManifest(Namespace):
+    version: str
+    time: str
+    launcher: Launcher
+    packages: Dict[str, IndexPackage] = field(default_factory=dict)
