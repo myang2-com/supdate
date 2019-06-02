212299aae59f37ede5d27a7945c988a8badf0e5e
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-06-02 16:11:32 +0900

    Add Package.to_json() override

--- supdate/package.py
+++ supdate/package.py
@@ -34,3 +34,17 @@ class Package(Profile):
     @classmethod
     def from_profile(cls, profile: Profile) -> Package:
         return cls.from_json(profile.to_json())
+
+    def to_json(self) -> dict:
+        prev_obj = super().to_json()
+        obj = {
+            "id": prev_obj.pop("id"),
+            "name": prev_obj.pop("name"),
+            "version": prev_obj.pop("version"),
+        }
+
+        files = prev_obj.pop("files")
+        obj.update(prev_obj)
+
+        obj["files"] = files
+        return obj
