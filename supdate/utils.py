400776bda9d25bee3caf2a2fda34cb98b7ac7238
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-06-02 16:07:37 +0900

    Add is_same_file()

--- supdate/utils.py
+++ supdate/utils.py
@@ -4,3 +4,10 @@ from pathlib import Path
 
 def sha256_hexdigest(file: Path):
     return _sha256(file.read_bytes()).hexdigest()
+
+
+def is_same_file(a: Path, b: Path):
+    if not a.exists() or not b.exists():
+        return False
+
+    return sha256_hexdigest(a) == sha256_hexdigest(b)
